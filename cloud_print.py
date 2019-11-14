d1edd886cf9991cb909cc35693a3f334b5b42bef
yurunyang1998 <674860268@qq.com>
2019-11-14 13:31:09 +0000

    add new printer, do not support old printer(kengdiede HP 2132)

--- cloud_print.py
+++ cloud_print.py
@@ -42,15 +42,14 @@ def lpr(filename, print_time=1):
     finalFile = filename
     try:
         suffix = filename.split('.')[-1]
-        if suffix == 'asdfasdf' or suffix == 'asdfadfasdf':
-            conv2pdf_file(filename)
-            # 转化完成后后缀应该是`*.pdf`
-
-            finalFile = filename.split('.')[0] + '.pdf'
-            os.popen(command.format('/home/pi/cp/', finalFile, print_time))
+        if suffix == 'pdf' or suffix == 'PDF':
+            # 文件是PDF时直接打印
+            command = 'lpr -P HP_LaserJet_1010 {}'
+            for i in range(print_time):
+                os.popen(command.format(uploader_dir + finalFile))
         else:
             print(command.format(base_dir + uploader_dir, finalFile, print_time))
-            for j in print_time:
+            for j in range(print_time):  #WARING: Don't try to fix the bug of print times, I have wasted whole night.... YURUNYANG & CHENJINHUA....  After 10mins CHEN tried again and failed . We shame on him ! 
                 print(j)
                 os.popen(command.format(uploader_dir, finalFile))
     except Exception as e:
@@ -74,7 +73,7 @@ def uploads():
         # print(upload_path)
         f.save(upload_path)
 
-        lpr(tempfile + '.' + filehouzhui, times)
+        lpr(tempfile + '.' + filehouzhui, int(times))
 
         # python使用os.pepon 来执行命令，也就是当前进程fork了个子进程来执行打印任务。
         # 也就是说当前需要有同步的方式，然而。。。
